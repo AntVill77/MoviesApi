40b08fa2b829966047058b74a9e393c2bb44111f
AntVill77 <antvillot77@hotmail.com>
2022-04-26 02:25:36 -0500

    creando el codigo del ViewModel y las corrutinas

--- app/src/main/java/org/example/MoviesApi/presentation/MovieViewModel.kt
+++ app/src/main/java/org/example/MoviesApi/presentation/MovieViewModel.kt
@@ -1,4 +1,37 @@
 package org.example.MoviesApi.presentation
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
+import org.example.MoviesApi.core.Resource
+import org.example.MoviesApi.repository.MovieRepository
+
+class MovieViewModel(private  val repo: MovieRepository): ViewModel() {
+
+    fun fetchTopRatedMovies() = liveData(Dispatchers.IO){
+        emit(Resource.Loading())
+
+        try {
+            emit(Resource.Success(repo.getTopRatedMovies()))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+
+    fun fetchPopularMovies() = liveData(Dispatchers.IO){
+        emit(Resource.Loading())
+
+        try {
+            emit(Resource.Success(repo.getPopularMovies()))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+}
+
+class MovieViewModelFactory(private val repo:MovieRepository): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
 }
--- app/src/main/java/org/example/MoviesApi/ui/movie/MovieFragment.kt
+++ app/src/main/java/org/example/MoviesApi/ui/movie/MovieFragment.kt
@@ -1,19 +1,58 @@
 package org.example.MoviesApi.ui.movie
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import org.example.MoviesApi.R
+import org.example.MoviesApi.core.Resource
+import org.example.MoviesApi.data.model.MovieList
+import org.example.MoviesApi.data.remote.MovieDataSource
 import org.example.MoviesApi.databinding.FragmentMovieBinding
+import org.example.MoviesApi.presentation.MovieViewModel
+import org.example.MoviesApi.presentation.MovieViewModelFactory
+import org.example.MoviesApi.repository.MovieRepositoryImpl
+import org.example.MoviesApi.repository.RetrofitClient
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
     private lateinit var binding: FragmentMovieBinding
+    private val viewModel by viewModels<MovieViewModel> {
+        MovieViewModelFactory(MovieRepositoryImpl(MovieDataSource(RetrofitClient.webservice)
+        )) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMovieBinding.bind(view)
 
-    }
+        viewModel.fetchTopRatedMovies().observe(viewLifecycleOwner, Observer { result ->
+          when(result){
+              is Resource.Loading -> {
+                 Log.d("LiveDataTopRated", "Loading...")
+              }
+              is Resource.Success -> {
+                  Log.d("LiveDataTopRated", "${result.data}")
+              }
+              is Resource.Failure -> {
+                  Log.d("Error", "${result.exception}")
+              }
+          }
+        })
 
+        viewModel.fetchPopularMovies().observe(viewLifecycleOwner, Observer { result ->
+            when(result){
+                is Resource.Loading -> {
+                    Log.d("LiveDataPopular", "Loading...")
+                }
+                is Resource.Success -> {
+                    Log.d("LiveDataPopular", "${result.data}")
+                }
+                is Resource.Failure -> {
+                    Log.d("Error", "${result.exception}")
+                }
+            }
+        })
+    }
 }
