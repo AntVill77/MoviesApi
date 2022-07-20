f7b6e36b53ae8d888f3ef9c5af38d08b308586c8
AntVill77 <antvillot77@hotmail.com>
2022-07-20 18:14:42 -0500

    subiendo branch

--- app/src/main/java/org/example/MoviesApi/repository/MovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.example.MoviesApi.repository
-
-import org.example.MoviesApi.data.model.MovieList
-
-interface MovieRepository {
-    suspend fun getTopRatedMovies(): MovieList
-    suspend fun getPopularMovies(): MovieList
-}
